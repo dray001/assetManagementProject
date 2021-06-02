76a2e80df5c4198abad3cd90c93bd20a0f86b85b
Dare Oni <dray@Dares-MacBook-Pro-2.local>
2021-06-02 12:15:24 +0100

    Database configuration with Atlas DB, integrating Mongoose to connect with DB

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.send('This is the Asset Manager API');
   });
+
+  const connect = async () => {
+    try {
+        await mongoose.connect(process.env.DB, {
+          useNewUrlParser: true, 
+          useUnifiedTopology: true
+        });
+        const conn = mongoose.connection;
+        console.log(`Connected to the ${conn.name} data`);
+      } catch (error) {
+        console.log('Error on db Connection - Code:', error.code);
+      }
+  };
+
+  connect();
+
+  
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
